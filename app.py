abe4af5c56f9594820ea6780d63e5633c8782462
KingAlex2220 <tfragrancesofficial@gmail.com>
2026-08-09 16:57:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ if access_mode == "🛍️ Public Storefront":
                 st.markdown("#### 4. Select Settlement Channel")
                 payment_method = st.selectbox(
                     "Payment / Settlement Channel:",
-                    ["Zelle", "Cash App", "Venmo", "Apple Pay / Text Payment"],
+                    ["Zelle"],
                     help="Choose your preferred payment method to view settlement details."
                 )
                 
