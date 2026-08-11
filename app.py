1b10714e40b2807065b9a5afe0b78dd5cc7a4dcb
KingAlex2220 <alexthomp80@gmail.com>
2026-08-11 07:28:14 -0400

    Fix markdown formatting in app.py

--- app.py
+++ app.py
@@ -712,7 +712,7 @@ st.markdown(
 )
 
 st.markdown(
-    """
+    """,
     <p style='text-align: center; color: #FFFFFF; font-size: 0.9rem; margin-top: 10px;'>
     © T Fragrances. All Rights Reserved.
   
