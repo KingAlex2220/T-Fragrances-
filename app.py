99cfdc1d72a5e0846f99b6b267e66e6a63f662dc
KingAlex2220 <tfragrancesofficial@gmail.com>
2026-08-11 17:53:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def restock_item(product_code, add_qty):
     conn.commit()
     conn.close()
 
-PRICE_PER_BOTTLE = 45.00
+PRICE_PER_BOTTLE = 60.00
 LOCAL_BOTTLE_IMG = "images/bottles.png"
 LOCAL_QR_IMG = "images/zelle_qr.png"
 LOCAL_CATALOG_QR_IMG = "images/Catalog_qr.png.jpg"
