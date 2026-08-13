c92604fbfcdfc0bc1cdf9617953465a905410bda
KingAlex2220 <alexthomp80@gmail.com>
2026-08-13 07:34:50 -0400

    Update title and admin password in app.py

--- app.py
+++ app.py
@@ -466,7 +466,7 @@ st.sidebar.subheader(f"Total: ${final_subtotal:.2f}")
 # ==========================================
 # MAIN INTERFACE
 # ==========================================
-st.title("T Fragrances POS & Master Portal")
+st.title("T Fragrances")
 
 st.info(
     "💡 **Looking for something else?**,"
@@ -979,7 +979,7 @@ with tabs[5]:
   st.header("🔒 Master Admin Database & Restocking Management")
   admin_pwd = st.text_input("Enter Admin Security Password", type="password")
 
-  if admin_pwd == "admin123":
+  if admin_pwd == "Saf9uard-tf80":
     st.success("Staff Authentication Verified")
 
     # --- PARTNERSHIP & AFFILIATE PERFORMANCE TRACKER ---
